b6b8499b5f5c20c9f026b36c0c118d6969f377b9
infobit <jorge@infobit.es>
2025-10-28 09:12:58 +0000

    Incorporación tracking a varios campos

--- l10n_es_aeat_verifactu/models/res_company.py
+++ l10n_es_aeat_verifactu/models/res_company.py
@@ -16,9 +16,9 @@ class res_company(models.Model):
     _inherit = "res.company"
 
     tax_agency_id = fields.Many2one("aeat.tax.agency", "AEAT Agency")
-    verifactu_enabled = fields.Boolean(string="Enable veri*FACTU")
-    verifactu_test = fields.Boolean(string="Is it the veri*FACTU test environment?")
-    verifactu_description = fields.Text(default="/", size=500, help="The description for Verifactu invoices if not set",)
+    verifactu_enabled = fields.Boolean(string="Enable veri*FACTU", tracking=True)
+    verifactu_test = fields.Boolean(string="Is it the veri*FACTU test environment?", tracking=True)
+    verifactu_description = fields.Text(default="/", size=500, help="The description for Verifactu invoices if not set", tracking=True)
     last_verifactu_invoice_entry_id = fields.Many2one(
         "verifactu.invoice.entry",
         string="VeriFactu Invoice Entry",
@@ -29,13 +29,16 @@ class res_company(models.Model):
         comodel_name="verifactu.developer",
         string="Verifactu Developer",
         ondelete="set null",
+        tracking=True,
     )
     verifactu_start_date = fields.Date(
+        tracking=True,
         help="If this field is set, the verifactu won't be enabled on invoices with lower "
         "invoice date. If not set, the verifactu can be enabled on all invoice dates"
     )
     verifactu_use_connector = fields.Boolean(
         string='Use connector',
+        tracking=True,
         help="Check it to use connector instead of sending the invoice "
              "directly when it's validated")
     verifactu_method = fields.Selection(
